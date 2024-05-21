cda02a1b191f03a183608e2d46c03733c185a6c3
Arghyadeep <105662359+K-Arghyadeep@users.noreply.github.com>
2024-05-21 23:59:25 +0530

    added 2 new beans

---
+++ backend/Fitness_Fusion_1/src/main/java/com/fitness/beans/User.java
@@ -0,0 +1,78 @@
+package com.fitness.beans;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_details")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "security_question", nullable = false)
+    private String security_question;
+    @Column(name = "answer", nullable = false)
+    private String answer;
+    @Column(name = "premium", columnDefinition = "char(1) default '0'")
+    private char premium = '0';
+    @Column(name = "otp")
+    private String otp;
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSecurity_question() {
+		return security_question;
+	}
+	public void setSscurity_question(String security_question) {
+		this.security_question = security_question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public char getPremium() {
+		return premium;
+	}
+	public void setPremium(char premium) {
+		this.premium = premium;
+	}
+	public String getOtp() {
+		return otp;
+	}
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+}
